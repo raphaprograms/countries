faeaba283a87fc4812178166c15ae9a8b4a0afb1
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-10-15 13:36:18 -0700

    Initialize project

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// For the Countries Project use the following endpoints:
+// Get all countries: `https://restcountries.com/v3.1/all?fields=name,population,region,capital`
+// All country details: `https://restcountries.com/v3.1/name/Lithuania?fullText=true`
+
+// For filtering use: `https://restcountries.com/v3.1/region/asia`
+// The value for name and region should be dynamic ^^^

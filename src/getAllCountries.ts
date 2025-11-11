da6da47867ccfcf75b5ae247dab97f717a9b7009
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-11-11 15:20:56 -0800

    Add types to getAllCountries module

--- src/getAllCountries.ts
+++ src/getAllCountries.ts
@@ -1,8 +1,29 @@
+export type RestCountryGrid = {
+  name: { common: string ; official: string };
+  flags:{ png?: string; svg?: string; alt?: string};
+  population?: number;
+  region: string[];
+  capital: string;
+  cca3?: string;
+};
+
+export type CountryCard = {
+  name: string;
+  flagsSrc: string;
+  flagAlt: string;
+  population: number;
+  region: string;
+  capital: string;
+  code: string;
+};
+
+const API = 'https://restcountries.com/v3.1';
+const gridFields = 'name,flags,population,region,capital,cca3'
 
 
-export default async function () {
+export async function getAllCountries(): Promise<RestCountryGrid[]> {
   const res = await fetch(
-    "https://restcountries.com/v3.1/all?fields=name,population,region,capital",
+    `${API}/all?fields=${gridFields}`,
   );
 
   if (!res.ok) {
@@ -11,4 +32,18 @@ export default async function () {
 
   const data = await res.json();
   console.log(data);
+  return data;
 };
+
+export function toCountryCard(countryData: RestCountryGrid): CountryCard {
+  return {
+    name: countryData.name.common,
+    flagsSrc: countryData.flags.png,
+    flagAlt: countryData.flags.alt,
+    population: countryData.population,
+    region: countryData.region,
+    capital: countryData.capital,
+    code: countryData.cca3,
+
+  };
+}
